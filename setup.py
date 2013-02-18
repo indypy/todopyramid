1240dd972e70424baf8535e6981c7ed56e8eeac0
Clayton Parker <clayton@sixfeetup.com>
2013-02-18 17:19:01 -0500

    adding deform_bootstrap

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ requires = [
     'pyramid_debugtoolbar',
     'zope.sqlalchemy',
     'waitress',
+    'deform_bootstrap',
 ]
 
 setup(
--- todopyramid/__init__.py
+++ todopyramid/__init__.py
@@ -15,6 +15,14 @@ def main(global_config, **settings):
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
+    # Adding the static resources from Deform
+    config.add_static_view(
+        'deform_static', 'deform:static', cache_max_age=3600
+    )
+    config.add_static_view(
+        'deform_bootstrap_static', 'deform_bootstrap:static',
+        cache_max_age=3600
+    )
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()

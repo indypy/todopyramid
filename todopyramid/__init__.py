e332ddab05cfcf5b598e273f65e5a96409420f57
Clayton Parker <clayton@sixfeetup.com>
2013-02-23 10:33:41 -0500

    include deform_bootstrap

--- todopyramid/__init__.py
+++ todopyramid/__init__.py
@@ -18,6 +18,7 @@ def main(global_config, **settings):
         root_factory='todopyramid.models.RootFactory',
     )
     config.include('pyramid_persona')
+    config.include('deform_bootstrap')
     config.add_static_view('static', 'static', cache_max_age=3600)
     # Adding the static resources from Deform
     config.add_static_view(

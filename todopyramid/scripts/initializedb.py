ee94eeb5e91985e387564a56df8932f00290984e
Sascha Gottfried <sascha.gottfried@googlemail.com>
2014-04-04 18:17:41 +0200

    revert to handling transaction outside of content creation function, because it fits to the existing two use-cases.

--- todopyramid/scripts/initializedb.py
+++ todopyramid/scripts/initializedb.py
@@ -18,6 +18,7 @@ from ..models import (
     Base,
     )
 
+from ..utils import localize_datetime
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
@@ -26,69 +27,83 @@ def usage(argv):
     sys.exit(1)
 
 
-def create_dummy_content():
-    """Create some tasks by default to show off the site
+def create_dummy_user():
+    """create our dummy user
+    
+    we handle transaction not here - design decision
     """
-    with transaction.manager:
 
-        user = TodoUser(
-            email=u'king.arthur@example.com',
-            first_name=u'Arthur',
-            last_name=u'Pendragon',
-        )
-        DBSession.add(user)  
-        user_id = user.email 
+    user = TodoUser(
+        email=u'king.arthur@example.com',
+        first_name=u'Arthur',
+        last_name=u'Pendragon',
+    )
+    DBSession.add(user)  
+    user_id = user.email 
+    return user_id
+
+def create_dummy_content(user_id):
+    """Create some tasks for this user with by default to show off the site 
+    
+    either called during application startup or during content creation while a new user registers
+    we do not handle transaction here - design decision  
+    
+    TODO: bulk adding of new content
+    """   
         
-        #this user creates several todo items   
-        task = TodoItem(
-            user=user_id,
-            task=u'Find a shrubbery',
-            tags=[u'quest', u'ni', u'knight'],
-            due_date=datetime.utcnow() + timedelta(days=60),
-        )
-        DBSession.add(task)
-        task = TodoItem(
-            user=user_id,
-            task=u'Search for the holy grail',
-            tags=[u'quest'],
-            due_date=datetime.utcnow() - timedelta(days=1),
-        )
-        DBSession.add(task)
-        task = TodoItem(
-            user=user_id,
-            task=u'Recruit Knights of the Round Table',
-            tags=[u'quest', u'knight', u'discuss'],
-            due_date=datetime.utcnow() + timedelta(minutes=45),
-        )
-        DBSession.add(task)
-        task = TodoItem(
-            user=user_id,
-            task=u'Build a Trojan Rabbit',
-            tags=[u'quest', u'rabbit'],
-            due_date=datetime.utcnow() + timedelta(days=1),
-        )
-        DBSession.add(task)
-        task = TodoItem(
-            user=user_id,
-            task=u'Talk to Tim the Enchanter',
-            tags=[u'quest', u'discuss'],
-            due_date=datetime.utcnow() + timedelta(days=90),
-        )
-        DBSession.add(task)
-        task = TodoItem(
-            user=user_id,
-            task=u'Defeat the Rabbit of Caerbannog',
-            tags=[u'quest', u'rabbit'],
-            due_date=None,
-        )
-        DBSession.add(task)
-        task = TodoItem(
-            user=user_id,
-            task=u'Cross the Bridge of Death',
-            tags=[u'quest'],
-            due_date=None,
-        )
-        DBSession.add(task)
+    user = DBSession.query(TodoUser).filter(TodoUser.email == user_id).first()
+    time_zone = user.time_zone 
+         
+    #this user creates several todo items with localized times
+    task = TodoItem(
+        user=user_id,
+        task=u'Find a shrubbery',
+        tags=[u'quest', u'ni', u'knight'],
+        due_date=localize_datetime((datetime.utcnow() + timedelta(days=60)), time_zone),
+    )
+    DBSession.add(task)
+    task = TodoItem(
+        user=user_id,
+        task=u'Search for the holy grail',
+        tags=[u'quest'],
+        due_date=localize_datetime((datetime.utcnow() + timedelta(days=1)), time_zone),
+    )
+    DBSession.add(task)
+    task = TodoItem(
+        user=user_id,
+        task=u'Recruit Knights of the Round Table',
+        tags=[u'quest', u'knight', u'discuss'],
+        due_date=localize_datetime((datetime.utcnow() + timedelta(minutes=45)), time_zone),
+    )
+    DBSession.add(task)
+    task = TodoItem(
+        user=user_id,
+        task=u'Build a Trojan Rabbit',
+        tags=[u'quest', u'rabbit'],
+        due_date=localize_datetime((datetime.utcnow() + timedelta(days=1)), time_zone),
+    )
+    DBSession.add(task)
+    task = TodoItem(
+        user=user_id,
+        task=u'Talk to Tim the Enchanter',
+        tags=[u'quest', u'discuss'],
+        due_date=localize_datetime((datetime.utcnow() + timedelta(days=90)), time_zone),
+    )
+    DBSession.add(task)
+    task = TodoItem(
+        user=user_id,
+        task=u'Defeat the Rabbit of Caerbannog',
+        tags=[u'quest', u'rabbit'],
+        due_date=None,
+    )
+    DBSession.add(task)
+    task = TodoItem(
+        user=user_id,
+        task=u'Cross the Bridge of Death',
+        tags=[u'quest'],
+        due_date=None,
+    )
+    DBSession.add(task)
 
 
 def main(argv=sys.argv):
@@ -100,6 +115,8 @@ def main(argv=sys.argv):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-    create_dummy_content()
+    with transaction.manager:
+        user_id = create_dummy_user()
+        create_dummy_content(user_id)
 
         
